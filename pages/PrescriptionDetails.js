0ac7bd5909098ead9277a9a284c8543fca8f8b60
SachinL98 <143057141+SachinL98@users.noreply.github.com>
2023-10-20 22:41:23 +0530

    prescription page

--- pages/PrescriptionDetails.js
+++ pages/PrescriptionDetails.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, ImageBackground } from 'react-native'; // Import ImageBackground
+import { View, Text, StyleSheet, Image, ImageBackground } from 'react-native';
 import { Card, Title, Paragraph, Button, Divider } from 'react-native-paper';
 import { useNavigation, useRoute } from '@react-navigation/native';
+
 
 const PrescriptionDetails = () => {
   const route = useRoute();
   const { date, doctorName, doctorType, doctorMobile, username } = route.params;
 
-  // Define a static prescription list for demonstration
   const prescriptionList = [
     {
       reason: 'Chest pain',
@@ -20,50 +20,59 @@ const PrescriptionDetails = () => {
       speciality: 'Cardiologist',
       advices: "Don't continue to use Panadoxin if you have any allergic reactions",
     },
-    // Add more prescriptions as needed
   ];
 
   return (
     <ImageBackground source={require('../assets/images/profilebackground.jpg')} style={styles.backgroundImage}>
       <View style={styles.container}>
-        <View style={styles.headerContainer}>
-          <Text style={styles.header}>Prescription</Text>
-        </View>
-        <View style={styles.doctorContainer}>
-          <Image
-            source={{ uri: 'https://randomuser.me/api/portraits/men/75.jpg' }}
-            style={styles.doctorImage}
-          />
-          <View style={styles.doctorDetails}>
-            <Text style={styles.title}>{doctorName}</Text>
-            <Text style={styles.subtitle}>{doctorType}</Text>
-            <Text style={styles.subtitle}>★ 4.7</Text>
-            <Text style={styles.subtitle}>{doctorMobile}</Text>
+        <View style={styles.mainContainer}>
+          <View style={styles.headerContainer}>
+            <Text style={styles.header}>Prescription</Text>
           </View>
+          <View style={styles.doctorContainer}>
+            <Image
+              source={{ uri: 'https://randomuser.me/api/portraits/men/75.jpg' }}
+              style={styles.doctorImage}
+            />
+            <View style={styles.doctorDetails}>
+              <Text style={styles.title}>{doctorName}</Text>
+              <Text style={styles.subtitle}>{doctorType}</Text>
+              <Text style={styles.subtitle}>★ 4.7</Text>
+              <Text style={styles.subtitle}>{doctorMobile}</Text>
+            </View>
+          </View>
+          <View style={styles.infoContainer}>
+            <View style={styles.transparentContainer}>
+              <Title style={styles.subtitle} >Patient - {username}</Title>
+            </View>
+            <View style={styles.transparentContainer}>
+              <Text style={styles.subtitle} >Prescription Date: {date}</Text>
+            </View>
+            <View style={styles.transparentContainer}>
+              <Text style={styles.subtitle} >Reason: {prescriptionList[0].reason}</Text>
+            </View>
+          </View>
+          {prescriptionList.map((prescription, index) => (
+            <Card key={index} style={styles.card}>
+              <Card.Content>
+                <Paragraph>Prescribed List:</Paragraph>
+                <Divider style={styles.divider} />
+                {prescription.drugs.map((drug, drugIndex) => (
+                  <View key={drugIndex} style={styles.drugRow}>
+                    <Text style={styles.drugName}>{`${drugIndex + 1}. ${drug.name}`}</Text>
+                    <Text style={styles.dosage}>{drug.dosage}</Text>
+                  </View>
+                ))}
+                <Divider style={styles.divider} />
+                <Paragraph>Special Advices: {prescription.advices}</Paragraph>
+              </Card.Content>
+            </Card>
+          ))}
         </View>
-        {prescriptionList.map((prescription, index) => (
-          <Card key={index} style={styles.card}>
-            <Card.Content>
-              <Title>Patient - {username}</Title>
-              <Text>Prescription Date: {date}</Text>
-              <Paragraph>Reason: {prescription.reason}</Paragraph>
-              <Paragraph>Prescribed List:</Paragraph>
-              <Divider style={styles.divider} />
-              {prescription.drugs.map((drug, drugIndex) => (
-                <View key={drugIndex} style={styles.drugRow}>
-                  <Text style={styles.drugName}>{`${drugIndex + 1}. ${drug.name}`}</Text>
-                  <Text style={styles.dosage}>{drug.dosage}</Text>
-                </View>
-              ))}
-              <Divider style={styles.divider} />
-              <Paragraph>Special Advices: {prescription.advices}</Paragraph>
-            </Card.Content>
-            <Card.Actions style={styles.cardActions}>
-              <Button onPress={() => {}}>Make PDF</Button>
-              <Button onPress={() => {}}>Send To Pharmacy</Button>
-            </Card.Actions>
-          </Card>
-        ))}
+        <View style={styles.buttonContainer}>
+                <Button onPress={() => {}} style={styles.button} >Make PDF</Button>
+                <Button onPress={() => {}} style={styles.button} >Send To Pharmacy</Button>
+              </View>
       </View>
     </ImageBackground>
   );
@@ -72,8 +81,14 @@ const PrescriptionDetails = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-start', // Align items at the top of the screen
+    justifyContent: 'center',
     alignItems: 'center',
+  },
+  mainContainer: {
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    borderRadius: 10,
+    padding: 10,
+    width: '90%',
   },
   headerContainer: {
     alignItems: 'center',
@@ -83,10 +98,33 @@ const styles = StyleSheet.create({
     fontSize: 25,
     fontWeight: 'bold',
     marginBottom: 10,
-    marginTop: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom:10,
+    borderRadius: 10,
+  },
+  button: {
+    marginHorizontal: 10,
+    backgroundColor: 'rgba(255, 255, 255, 0.9)', // Add horizontal margin for buttons
+    elevation: 10,
+    padding: 5,
+    marginBottom:5,
+  },
+  infoContainer: {
+    marginBottom: 5,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  transparentContainer: {
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 5,
+    fontWeight: 'bold',
   },
   card: {
-    width: '80%',
     elevation: 4,
     marginBottom: 10,
   },
@@ -96,10 +134,7 @@ const styles = StyleSheet.create({
   },
   doctorContainer: {
     flexDirection: 'row',
-    backgroundColor: 'rgba(255, 255, 255, 0.7)',
     padding: 10,
-    borderRadius: 10,
-    width: '70%', // Set the width to 70% of the screen width
     marginBottom: 10,
   },
   doctorImage: {
@@ -116,8 +151,9 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
   subtitle: {
-    fontSize: 14,
+    fontSize: 18,
     marginBottom: 5,
+    fontWeight: 'bold',
   },
   divider: {
     marginTop: 10,
@@ -133,13 +169,16 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     marginBottom: 5,
+    fontWeight: 'bold',
   },
   drugName: {
     flex: 1,
+    fontWeight: 'bold',
   },
   dosage: {
     flexBasis: '30%',
     textAlign: 'right',
+    fontWeight: 'bold',
   },
 });
 
