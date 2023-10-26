c73b48d98254f05546af08be5b7e9fb4758e2b52
Amisha Prathyanga <spbamisha@gmail.com>
2023-10-26 16:33:19 +0530

    profile changes

--- pages/Profile.js
+++ pages/Profile.js
@@ -1,12 +1,22 @@
-import React, {useEffect} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  FlatList,
+  Alert,
+} from 'react-native';
 import {useFocusEffect} from '@react-navigation/native';
 import {getUserSession} from '../SessionManager/SessionManager';
 import {getDatabase, ref, get, set} from 'firebase/database';
 
-const Profile = () => {
+const Profile = ({navigation}) => {
   const database = getDatabase();
   const [userData, setUserData] = React.useState({});
+
+  const userType = userData.Type;
 
   useEffect(() => {
     const checkUserSession = async () => {
@@ -36,9 +46,70 @@ const Profile = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Profile</Text>
-      {userData.Name && <Text style={styles.text}>Name: {userData.Name}</Text>}
-      {userData.Type && <Text style={styles.text}>Name: {userData.Type}</Text>}
+      <Image
+        source={{uri: 'https://www.bootdey.com/image/900x400/FF7F50/000000'}}
+        style={styles.coverImage}
+      />
+      <View style={styles.avatarContainer}>
+        <Image
+          source={{
+            uri: 'https://www.bootdey.com/img/Content/avatar/avatar1.png',
+          }}
+          style={styles.avatar}
+        />
+        <Text style={[styles.name, styles.textWithShadow]}>
+          {userData.Name}
+        </Text>
+      </View>
+      <View style={styles.content}>
+        <View style={styles.infoContainer}>
+          <Text style={styles.infoLabel}>Mobile:</Text>
+          <Text style={styles.infoValue}>{userData.Mobile}</Text>
+        </View>
+        <View style={styles.infoContainer}>
+          <Text style={styles.infoLabel}>Location:</Text>
+          <Text style={styles.infoValue}>{userData.Location}</Text>
+        </View>
+        <View style={styles.infoContainer}>
+          <Text style={styles.infoLabel}>Blood Type:</Text>
+          <Text style={styles.infoValue}>{userData.BloodType}</Text>
+        </View>
+      </View>
+      {userType === 'user' && (
+        <View style={styles.buttonContain}>
+          <TouchableOpacity
+            style={styles.buttonContainer}
+            onPress={() => navigation.navigate('RequestBlood')}>
+            <Text>Add Blood Request</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.buttonContainer}>
+            <Text>Previously added Requests</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.buttonContainer}
+            onPress={() => navigation.navigate('AcceptedRequestsPage')}>
+            <Text>View Accepted Requests</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+
+      {userType === 'donor' && (
+        <View style={styles.buttonContain}>
+          <TouchableOpacity style={styles.buttonContainer} onPress={() => navigation.navigate('BloodRequestPage')}>
+            <Text>View Blood Request Feed</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.buttonContainer} onPress={() => navigation.navigate('DonorAcceptedRequests')}>
+            <Text>View Accepted Requests</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.buttonContainer}>
+            <Text>Donor-specific Button 3</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 };
@@ -46,20 +117,72 @@ const Profile = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    backgroundColor: '#fff',
+    padding: 20,
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginTop: 16,
-    color: '#13BC9E',
-  },
+  // title: {
+  //   fontSize: 24,
+  //   fontWeight: 'bold',
+  //   marginTop: 16,
+  //   color: '#13BC9E',
+  // },
 
-  text: {
-    marginTop: 25,
+  // text: {
+  //   marginTop: 25,
+  //   fontSize: 20,
+  //   fontWeight: 'bold',
+  //   color: '#13BC9E',
+  // },
+  coverImage: {
+    height: 200,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+  },
+  avatarContainer: {
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  avatar: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+  },
+  name: {
     fontSize: 20,
     fontWeight: 'bold',
-    color: '#13BC9E',
+    marginTop: 10,
+    color: 'white',
+  },
+  content: {
+    marginTop: 20,
+  },
+  infoContainer: {
+    marginTop: 20,
+  },
+  infoLabel: {
+    fontWeight: 'bold',
+  },
+  infoValue: {
+    marginTop: 5,
+  },
+  buttonContainer: {
+    marginTop: 10,
+    height: 45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30,
+    backgroundColor: '#00CED1',
+  },
+
+  buttonContain: {
+    marginTop: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
