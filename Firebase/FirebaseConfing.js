2b697da6176c365a780f3164c7b4acb7eae77b72
Pasindu Shyaminda <it21016202@my.sliit.lk>
2023-10-30 21:47:39 +0530

    Update FirebaseConfing.js

--- Firebase/FirebaseConfing.js
+++ Firebase/FirebaseConfing.js
@@ -3,23 +3,9 @@ import {getStorage} from 'firebase/storage';
 import {ref as storageRef} from 'firebase/storage';
 
 const FirebaseConfig = {
-  apiKey: 'AIzaSyA7U0nYAIxzqsnf4zsjcICspP5VLOuR_t8',
-  authDomain: 'medifind-ad39c.firebaseapp.com',
-  databaseURL: 'https://medifind-ad39c-default-rtdb.firebaseio.com',
-  projectId: 'medifind-ad39c',
-  storageBucket: 'medifind-ad39c.appspot.com',
-  messagingSenderId: '261385207351',
-  appId: '1:261385207351:android:15bb11ff9fcd7c09d849b2',
+  //
 };
-// const FirebaseConfig = {
-//   apiKey: 'AIzaSyA7U0nYAIxzqsnf4zsjcICspP5VLOuR_t8',
-//   authDomain: 'medifind-ad39c.firebaseapp.com',
-//   databaseURL: 'https://medifind-ad39c-default-rtdb.firebaseio.com',
-//   projectId: 'medifind-ad39c',
-//   storageBucket: 'medifind-ad39c.appspot.com',
-//   messagingSenderId: '261385207351',
-//   appId: '1:261385207351:android:15bb11ff9fcd7c09d849b2',
-// };
+
 
 const app = initializeApp(FirebaseConfig);
 
